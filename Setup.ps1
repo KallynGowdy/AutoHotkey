b496c888f6e66996a755426a662c3fc6ceecadf4
Kallyn Gowdy <kallyngowdy@gmail.com>
2024-10-24 12:59:12 -0400

    fix: Scheduled task for starting AutoHotkey script

--- Setup.ps1
+++ Setup.ps1
@@ -5,8 +5,15 @@ winget import -i ./windows.apps.json --accept-package-agreements --accept-source
 
 Write-Host "Setting Up AutoHotKey Task"
 # get path from script directory
-$ahkPath = "$PSScriptRoot\Toggle.ahk"
-$ahkTask = New-ScheduledTaskAction -Execute $ahkPath
+$ahkPath = "C:\Program Files\AutoHotkey\UX\AutoHotkeyUX.exe"
+$launcherPath = "C:\Program Files\AutoHotkey\UX\launcher.ahk"
+$togglePath = "$PSScriptRoot\Toggle.ahk"
+$ahkTask = New-ScheduledTaskAction -Execute $ahkPath -Argument """$launcherPath"" /Launch ""$togglePath"""
 $ahkTrigger = New-ScheduledTaskTrigger -AtLogOn
 $ahkSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+
+$existingTask = Get-ScheduledTask -TaskName "AutoHotKey" -ErrorAction SilentlyContinue
+if ($existingTask) {
+    Unregister-ScheduledTask -TaskName "AutoHotKey" -Confirm:$false
+}
 Register-ScheduledTask -TaskName "AutoHotKey" -Action $ahkTask -Trigger $ahkTrigger -Settings $ahkSettings
