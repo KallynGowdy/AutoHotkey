10acceaea5dab9a8f63abafb51c25f5667dc30a3
Kallyn Gowdy <kallyngowdy@gmail.com>
2024-10-24 11:41:56 -0400

    feat: Add a task to automatically start AutoHotKey

--- Setup.ps1
+++ Setup.ps1
@@ -1,1 +1,12 @@
+#Requires -RunAsAdministrator
+
+Write-Host "Installing Packages"
 winget import -i ./windows.apps.json --accept-package-agreements --accept-source-agreements
+
+Write-Host "Setting Up AutoHotKey Task"
+# get path from script directory
+$ahkPath = "$PSScriptRoot\Toggle.ahk"
+$ahkTask = New-ScheduledTaskAction -Execute $ahkPath
+$ahkTrigger = New-ScheduledTaskTrigger -AtLogOn
+$ahkSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+Register-ScheduledTask -TaskName "AutoHotKey" -Action $ahkTask -Trigger $ahkTrigger -Settings $ahkSettings
